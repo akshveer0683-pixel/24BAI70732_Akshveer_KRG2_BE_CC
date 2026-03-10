f859f450790ba1d4a3323d7dc1fcaab45808db9b
Akshveer Singh Lamba <akshveer0683@gmail.com>
2026-03-10 09:31:52 +0530

    Create experiment3.cpp

---
+++ Experiment 3/experiment3.cpp
@@ -0,0 +1,52 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> result;
+    vector<string> board;
+    int n;
+
+    bool issafe(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void solve(int row) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (issafe(row, col)) {
+                board[row][col] = 'Q';
+                solve(row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        this->n = n;
+        board = vector<string>(n, string(n, '.'));
+        solve(0);
+        return result;
+    }
+};
+
